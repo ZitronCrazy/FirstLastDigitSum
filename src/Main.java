8549d93b8ed5034e0964d2fb378ec00f0543a111
ZitronCrazy <zitro5@protonmail.com>
2021-06-27 00:38:00 +0200

    FirstLastDigitSum

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        FirstLastDigitSum.sumFirstAndLastDigit(252);
+        FirstLastDigitSum.sumFirstAndLastDigit(257);
+        FirstLastDigitSum.sumFirstAndLastDigit(0);
+        FirstLastDigitSum.sumFirstAndLastDigit(5);
+        FirstLastDigitSum.sumFirstAndLastDigit(-10);
+    }
+}
